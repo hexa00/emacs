79d115743854845a4380382f99969108530e15be
Antoine Tremblay <antoine.tremblay@ericsson.com>
2017-04-07 10:29:59 -0400

    Use tide for typescript dev

--- init.el
+++ init.el
@@ -431,6 +431,34 @@ contextual information."
 (use-package jsx-mode
   :ensure t)
 
+;; https://github.com/ananthakumaran/tide
+(use-package tide
+  :ensure t
+  :config
+  (defun setup-tide-mode ()
+    (interactive)
+    (tide-setup)
+    (flycheck-mode +1)
+    (setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (eldoc-mode +1)
+    (tide-hl-identifier-mode +1)
+    ;; company is an optional dependency. You have to
+    ;; install it separately via package-install
+    ;; `M-x package-install [ret] company`
+    (company-mode +1)
+    ;; indent on tab with tide-format
+    (local-set-key (kbd "TAB") 'tide-format)
+  )
+
+  ;; aligns annotation to the right hand side
+  (setq company-tooltip-align-annotations t)
+
+  ;; formats the buffer before saving
+  (add-hook 'before-save-hook 'tide-format-before-save)
+  (add-hook 'typescript-mode-hook #'setup-tide-mode)
+  (add-hook 'js2-mode-hook #'setup-tide-mode)1
+  )
+
 (use-package work
   :ensure f)
 
